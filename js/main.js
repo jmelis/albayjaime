c13037591c623a1198874471214470df0a8dc132
Jaime Melis <jmelis@opennebula.org>
2012-05-20 23:35:51 +0200

    Make navigation work

--- js/main.js
+++ js/main.js
@@ -1,2 +1,27 @@
+function link_handler(id) {
+    var section_label;
+
+    switch(id)
+    {
+    case "cuanddonde":
+        section_label = "Cu&aacute;ndo y D&oacute;nde";
+        break;
+    case "alojamiento":
+        section_label = "Alojamiento";
+        break;
+    case "comollegar":
+        section_label = "C&oacute;mo Llegar";
+        break;
+    }
+
+    $(".content_wrapper").hide();
+    $("#sectiontitle").html(section_label).show();
+    $("#" + id + "_div").show();
+}
+
 $(document).ready(function() {
+    $("#navigation_pri li").click(function (){
+        var id = $(this).attr('id');
+        link_handler(id);
+    });
 });
